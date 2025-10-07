db2a27372f296b31b18f154b4a5b56ec12aa966f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-10-07 11:47:31 +0100

    Moved driver code

--- oop-2025-10-07-b-strings-files/Drivers/StringDriver.cs
+++ oop-2025-10-07-b-strings-files/Drivers/StringDriver.cs
@@ -1,11 +1,12 @@
-﻿using oop_2025_10_07_b_strings_files.Repositories;
+﻿using oop_2025_10_07_b_strings_files.Models;
+using oop_2025_10_07_b_strings_files.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace oop_2025_10_07_b_strings_files.Models
+namespace oop_2025_10_07_b_strings_files.Drivers
 {
     public static class StringDriver
     {
--- oop-2025-10-07-b-strings-files/Program.cs
+++ oop-2025-10-07-b-strings-files/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using oop_2025_10_07_b_strings_files.Drivers;
 using oop_2025_10_07_b_strings_files.Models;
 
 Console.WriteLine(AppData.AppName);
