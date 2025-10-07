1acd9330a44cb5c9c1c0d163c800fb28baeddce4
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-10-07 12:11:05 +0100

    FileDriver added

---
+++ oop-2025-10-07-b-strings-files/Drivers/FileDriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2025_10_07_b_strings_files.Drivers
+{
+    public static class FileDriver
+    {
+        public static void Run()
+        {
+            Console.WriteLine("File Driver Running...");
+            WriteTextFile();
+        } // end of Run()
+
+        public static void WriteTextFile() {
+
+            string [] lines = { "First line", "Second line", "Third line","Fourth Line" };
+            string fileToWrite = "../../../Documents/output.txt";
+            File.WriteAllLines(fileToWrite, lines);
+            Console.WriteLine($"Wrote to file: {fileToWrite}");
+
+        }
+    }
+}
--- oop-2025-10-07-b-strings-files/Program.cs
+++ oop-2025-10-07-b-strings-files/Program.cs
@@ -5,5 +5,6 @@ using oop_2025_10_07_b_strings_files.Models;
 
 Console.WriteLine(AppData.AppName);
 Console.WriteLine(AppData.AppVersion);
-MovieDriver.Run();
+FileDriver.Run();
+//MovieDriver.Run();
 //StringDriver.Run();
